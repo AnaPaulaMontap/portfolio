efa96c60d333b344f471984e563e27039308daa3
AnaPaulaMOntap <anapaula.monsalvet@gmail.com>
2019-04-03 01:59:19 -0300

    Updates

---
+++ precache-manifest.9a8eb1801a91aec80b4ce4b437b22b0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb83e1e5e85cff9812ca",
+    "url": "/portfolio/static/css/main.b9a719fb.chunk.css"
+  },
+  {
+    "revision": "cb83e1e5e85cff9812ca",
+    "url": "/portfolio/static/js/main.233fa0d8.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "49b709c4a957500685c8",
+    "url": "/portfolio/static/js/2.98da7d3a.chunk.js"
+  },
+  {
+    "revision": "4b9b28e58fb8609d87156c77c3e4ae5d",
+    "url": "/portfolio/static/media/yo.4b9b28e5.png"
+  },
+  {
+    "revision": "d426bdd4c2edb11231a395a7d787be01",
+    "url": "/portfolio/index.html"
+  }
+];
